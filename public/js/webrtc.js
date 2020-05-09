f46e5f5b39f474024679cebc78dbd98ab9c66d10
rajibpanja <rajibpanja@live.com>
2020-05-09 15:46:56 +0530

    UI enhnacement4

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -39,10 +39,7 @@ function hangUp() {
 //Open Microphone and Camera from your browser
 async function openUserMedia(e) {
   const stream = await navigator.mediaDevices.getUserMedia({
-    video: {
-      width: { min: 1024, ideal: 1280, max: 1920 },
-      height: { min: 576, ideal: 720, max: 1080 }
-    },
+    video:true,
     audio: true,
   });
 
